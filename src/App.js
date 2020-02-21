66fa57d92d3dd97fb8e1f061a11d137a2c017bb9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-21 11:01:38 -0500

    [+] C > QuotationForm

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import styled from '@emotion/styled';
 /** Components */
 import Header from './components/Header';
+import QuotationForm from './components/QuotationForm';
 
 /** Style Component div */
 const ContentComponent = styled .div `
     margin: 0 auto;
     max-width: 600px;
+`;
+
+const FormComponent = styled .div `
+    background-color: #d4e9ee;
+    padding: 2rem;
 `;
 
 function App() {
@@ -15,6 +21,10 @@ function App() {
             <Header 
                 title="Cotizador de Seguros para Automóvil"
             />
+        
+            <FormComponent>
+                <QuotationForm />
+            </FormComponent>
         </ContentComponent>
     );
 }
