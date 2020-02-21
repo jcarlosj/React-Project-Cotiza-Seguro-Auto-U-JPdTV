6568bcba04158c93d8ed614f6d338f6d577a12a5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-21 09:50:38 -0500

    [+] NPM >  @emotion/styled @emotion/core: Implementa Style Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
+/** Style Component Header (Crea 'Componente' de estilo) indicando el nombre de la etiqueta a desplegar */
+const ContentHeader = styled .header `
+    background-color: #26C6DA;
+    color: #FFFFFF;
+    font-weight: bold;
+    padding: 1rem 2rem;
+`;
+/** Style Component H1 */
+const ContentH1 = styled .h1 `
+    font-family: 'Slabo 27px', serif;
+    font-size: 2rem;
+    margin: 0;
+    text-align: center;
+`;
+
+/** Header Component */
 const Header = ({ title }) => {     // Destructuracion del props
     return (
-        <header>
-            <h1>{ title }</h1>
-        </header>
+        <ContentHeader>
+            <ContentH1>{ title }</ContentH1>
+        </ContentHeader>
     );
 }
 
